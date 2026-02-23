881ce6b61dc74fca9f371250cae50469dd48452f
Saad7890-web <omysaadislam@gmail.com>
2026-02-24 02:16:31 +0600

    uupdate organizations model added relationship

--- app/models/organization.py
+++ app/models/organization.py
@@ -4,6 +4,8 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
+from sqlalchemy import ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
 
 
 class Organization(Base):
@@ -15,6 +17,13 @@ class Organization(Base):
         default=uuid.uuid4,
     )
 
+    plan_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("plans.id"),
+        nullable=False,
+    )
+
     name: Mapped[str] = mapped_column(String(255), nullable=False)
 
     api_keys = relationship("APIKey", back_populates="organization")
+    plan = relationship("Plan", back_populates="organizations")
